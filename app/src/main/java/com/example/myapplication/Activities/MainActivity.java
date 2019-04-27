96ef7322c059f06d08a3a4634c2776eab1860236
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-04-27 12:33:03 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/Activities/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.myapplication.Activities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder builder;
+    private AlertDialog alertDialog;
+    private EditText itemName;
+    private EditText itemQuantity;
+    private Button saveButton;
+    private Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createPopupDialog();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void createPopupDialog(){
+
+        builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup,null);
+        itemName = (EditText) view.findViewById(R.id.itemNameID);
+        itemQuantity = (EditText) view.findViewById(R.id.itemQuantityID);
+        saveButton = (Button) view.findViewById(R.id.save_item_button);
+        builder.setView(view);
+        alertDialog = builder.create();
+        alertDialog.show();
+
+        //On click listener or Save button;
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveItemToDB(v);
+            }
+        });
+
+    }
+
+    private void saveItemToDB(View v) {
+
+    }
+}
