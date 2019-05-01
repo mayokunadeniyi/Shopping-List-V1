7600fbabf209fd6866ec624a74c1237bd82e1e56
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-05-01 07:52:56 +0100

    Edited UI: ListActivity, MainActivity

--- app/src/main/java/com/example/myapplication/Activities/ListActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/ListActivity.java
@@ -44,8 +44,6 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
